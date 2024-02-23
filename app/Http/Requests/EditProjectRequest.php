7dfb612e80afd463e7449d7f683d268b5adfd44a
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-23 19:14:49 +0100

    Verification per le tecnologie, da selezionarne almeno 1

--- app/Http/Requests/EditProjectRequest.php
+++ app/Http/Requests/EditProjectRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Technology;
 use Illuminate\Foundation\Http\FormRequest;
 
 class EditProjectRequest extends FormRequest
@@ -26,6 +27,9 @@ class EditProjectRequest extends FormRequest
         return [
             'title' => 'required|string|max:255',
             'description' => 'required|string',
+            'technology_id' => 'required|array',
+            'technology_id.*' => 'exists:technologies,id',
+
         ];
     }
     public function messages()
@@ -34,6 +38,7 @@ class EditProjectRequest extends FormRequest
             'title.required' => 'Il campo Title è richiesto',
             'title.max' => 'Il campo contiene un massimo di 255 caratteri',
             'description' => 'Il campo Description è richiesto',
+            'technology_id' => 'Devi selezionare almeno una Tecnologia'
         ];
     }
 }

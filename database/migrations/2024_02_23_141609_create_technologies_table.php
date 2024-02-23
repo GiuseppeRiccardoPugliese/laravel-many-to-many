a341b47a8653c4142b7a6c2f8294da6c20ae3885
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-23 15:34:38 +0100

    Recreate the migration for technologies

--- database/migrations/2024_02_23_141609_create_technologies_table.php
+++ database/migrations/2024_02_23_141609_create_technologies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('technologies_table_', function (Blueprint $table) {
+        Schema::create('technologies', function (Blueprint $table) {
             $table->id();
 
             $table->string('name');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('technologies_table_');
+        Schema::dropIfExists('technologies');
     }
 };

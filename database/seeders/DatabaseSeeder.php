4e82ca15e36e6183e07a39a255645c2745a5893a
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-23 16:00:26 +0100

    Creazione factory + seeder per la tabella Technology

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypeTableSeeder::class,
             ProjectTableSeeder::class,
+            TechnologyTableSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/TechnologyTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Technology;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TechnologyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Technology::factory()
+            ->count(10)
+            ->create()
+            ->each(function ($technology) {
+
+                $projects = Project::inRandomOrder()->limit(3)->get();
+                $technology->projects()->attach($projects);
+                $technology->save();
+            });
+    }
+}

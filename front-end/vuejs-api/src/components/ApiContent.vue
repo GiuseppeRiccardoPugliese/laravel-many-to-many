4fdb42b60613b423f9cb3800bff8d53157b6918f
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-28 17:19:42 +0100

    Chiamata axios per il test iniziale

---
+++ front-end/vuejs-api/src/components/ApiContent.vue
@@ -0,0 +1,30 @@
+<script>
+
+//Importo AXIOS
+import axios from 'axios';
+
+export default {
+    name: 'ApiContent',
+
+
+
+    mounted() {
+
+        axios.get('http://localhost:8000/api/v1/test')
+            .then(res => {
+
+                const data = res.data;
+                console.log(data);
+
+            })
+            .catch(err => {
+                console.err(err);
+            })
+
+    }
+}
+</script>
+
+<template>
+    <h1>hello world from api content!</h1>
+</template>

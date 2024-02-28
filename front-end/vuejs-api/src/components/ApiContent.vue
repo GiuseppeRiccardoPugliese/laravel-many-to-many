3da96d983038ed7708312e5956eec1119e6e6240
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-28 17:28:01 +0100

    listare tutte le tecnologie presenti in db

--- front-end/vuejs-api/src/components/ApiContent.vue
+++ front-end/vuejs-api/src/components/ApiContent.vue
@@ -6,16 +6,21 @@ import axios from 'axios';
 export default {
     name: 'ApiContent',
 
-
+    data() {
+        return {
+            technologies: [],
+        }
+    },
 
     mounted() {
 
-        axios.get('http://localhost:8000/api/v1/test')
+        axios.get('http://localhost:8000/api/v1/technologies')
             .then(res => {
 
                 const data = res.data;
-                console.log(data);
 
+                if (data.status == 'success')
+                    this.technologies = data.technologies;
             })
             .catch(err => {
                 console.err(err);
@@ -26,5 +31,11 @@ export default {
 </script>
 
 <template>
-    <h1>hello world from api content!</h1>
+    <h1>Technologies: </h1>
+
+    <ul>
+        <li v-for="technology in technologies" :key="technology.id">
+            <h2>{{ technology.name }}</h2>
+        </li>
+    </ul>
 </template>
